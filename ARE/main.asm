6858af977182b01dfe85af27887238502573524d
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-08-09 14:20:53 +0200

    Add old project files

--- ARE/main.asm
+++ ARE/main.asm
@@ -1,4 +1,47 @@
+.include "m2560def.inc"
+.include "distance_sens.inc"
 
-start:
-    inc r16
-    rjmp start
+.equ FOSC = 16000000
+.equ RESET_ADDR = 0
+
+.org RESET_ADDR
+rjmp l_reset
+
+.org INT_VECTORS_SIZE
+
+.include "led.asm"
+.include "uart.asm"
+.include "distance_sens.asm"
+
+l_reset:
+	;setup stack
+	ldi r16, HIGH(RAMEND)
+	out SPH, r16
+	ldi r16, LOW(RAMEND)
+	out SPL, r16
+	;setup power reduction
+	ldi r16, (1 << PRTIM2)
+	sts PRR0, r16
+	ser r16
+	sts PRR1, r16
+	;disable JTAG
+	in r16, MCUCR
+	ori r16, 1 << JTD 
+	out MCUCR, r16
+	;setup modules
+	UART_SR_SETUP
+	LED_SR_SETUP
+	DSENS_SR_SETUP
+	;turn off led
+	call led_sr_off
+	;set interrupts
+	SEI
+
+l_loop:	
+	; lds r16, TCNT1L
+	; lds r17, TCNT1H
+	; UART_SR_I r17 
+	; UART_SR_CI ':'
+	; UART_SR_I r16
+	; UART_SR_L 
+	rjmp l_loop
