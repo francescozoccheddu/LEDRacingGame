39135f93ad798523d649c3247f9ca1d0b138f395
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-08-15 00:34:19 +0200

    New UART

--- ARE/main.asm
+++ ARE/main.asm
@@ -4,8 +4,8 @@ l_reset:
 	SEI
 
 l_loop:	
-	UC_SR_C ds_out_l
-	UC_SR_C ds_out_r
+	UC_SR ds_out_l
+	UC_SR ds_out_r
 	rjmp l_loop
 
 #endif
--- ARE/src/uart_comm.asm
+++ ARE/src/uart_comm.asm
@@ -1,9 +1,6 @@
 #ifdef MACROS
 ;################# MACROS #################
 
-	.def uc_int = r24
-	.def uc_char = r25
-
 	.set UC_MACROS_ENABLED = 1
 
 	.equ UC_BAUD_RATE = 9600
@@ -20,78 +17,26 @@
 	.equ UC_UDR = UDR0
 	.equ UC_UCSZ = UCSZ00
 
-	;params (0)'immediate char'
-	.macro UC_SR_CI
-	.if UC_MACROS_ENABLED
-		push uc_char
-		ldi uc_char, @0
-		call uc_sr_send
-		pop uc_char
-	.endif
-	.endmacro
+;params (0)'register'
+.macro UC_SR
+	push @0
+	lds @0, UC_UCSRA
+	sbrs @0, UC_UDRE
+	rjmp PC - 3
+	pop @0
+	sts UC_UDR, @0
+.endmacro
 
-	;params (0)'char register'
-	.macro UC_SR_C
-	.if UC_MACROS_ENABLED
-		push uc_char
-		mov uc_char, @0
-		call uc_sr_send
-		pop uc_char
-	.endif
-	.endmacro
-
-	.macro UC_SR_L
-	.if UC_MACROS_ENABLED
-		UC_SR_CI '\n'
-	.endif
-	.endmacro
-
-	;params (0)'int register'
-	.macro UC_SR_I
-	.if UC_MACROS_ENABLED
-		push uc_int
-		mov uc_int, @0
-		call uc_sr_send_int
-		pop uc_int
-	.endif
-	.endmacro
-
-	;params (0)'immediate int'
-	.macro UC_SR_II
-	.if UC_MACROS_ENABLED
-		push uc_int
-		ldi uc_int, @0
-		call uc_sr_send_int
-		pop uc_int
-	.endif
-	.endmacro
-
-	;params (0)'str label'
-	.macro UC_SR_STR
-	.if UC_MACROS_ENABLED
-		push ZH
-		push ZL
-		ldi ZH, HIGH(2 * @0)
-		ldi ZL, LOW(2 * @0)
-		call uc_sr_send_str
-		pop ZL
-		pop ZH
-	.endif
-	.endmacro
-
-	;params (0)'unique id' (1)'immediate str'
-	.macro UC_SR_STRI_N
-	.if UC_MACROS_ENABLED
-		rjmp UC_L_STRI_CODE@0
-		.set UC_L_STRI_STR@0 = PC
-		.db @1, 0
-		.set UC_L_STRI_CODE@0 = PC
-		UC_SR_STR UC_L_STRI_STR@0
-	.endif
-	.endmacro
-
-;params (0)'immediate str'
-#define UC_SR_STRI UC_SR_STRI_N __LINE__ ,
+;params (0)'immediate'
+.macro UC_SR_I
+	push r16
+	lds r16, UC_UCSRA
+	sbrs r16, UC_UDRE
+	rjmp PC - 3
+	ldi r16, @0
+	sts UC_UDR, r16
+	pop r16
+.endmacro
 
 ;##########################################
 #endif
@@ -120,66 +65,7 @@
 #ifdef CODE
 ;################## CODE ##################
 
-;params (char)'char to send'
-uc_sr_send:
-	push uc_char
-uc_l_send_loop:
-	lds uc_char, UC_UCSRA
-	sbrs uc_char, UC_UDRE
-	rjmp uc_l_send_loop
-	pop uc_char
-	sts UC_UDR, uc_char
-	ret
 
-;params (Z)'address of the first character of a null-terminated string'
-uc_sr_send_str:
-	push uc_char
-uc_l_send_str_loop:
-	lpm uc_char, Z+
-	cpi uc_char, 0
-	breq uc_l_send_str_return
-	rcall uc_sr_send
-	rjmp uc_l_send_str_loop
-uc_l_send_str_return:
-	pop uc_char
-	ret
-
-;params (int)'int to send'
-uc_sr_send_int:
-	push uc_int
-	push math_dvd_res
-	push math_dvr_rem
-	push uc_char
-	mov math_dvd_res, uc_int
-	ldi math_dvr_rem, 100
-	rcall math_sr_div8
-	mov uc_int, math_dvd_res
-	cpi math_dvd_res, 0
-	breq uc_l_send_int_nd
-	mov uc_char, math_dvd_res
-	subi uc_char, -'0'
-	rcall uc_sr_send
-uc_l_send_int_nd:
-	mov math_dvd_res, math_dvr_rem
-	ldi math_dvr_rem, 10
-	rcall math_sr_div8
-	cpi math_dvd_res, 0
-	brne uc_l_send_int_nd_f
-	cpi uc_int, 0
-	breq uc_l_send_int_rd
-uc_l_send_int_nd_f:
-	mov uc_char, math_dvd_res
-	subi uc_char, -'0'
-	rcall uc_sr_send
-uc_l_send_int_rd:
-	mov uc_char, math_dvr_rem
-	subi uc_char, -'0'
-	rcall uc_sr_send
-	pop uc_char
-	pop math_dvr_rem
-	pop math_dvd_res
-	pop uc_int
-	ret
 
 ;##########################################
 #endif
