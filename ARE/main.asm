4c2d14abe8274fb64e24a32f02ad3ab07db63b51
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-08-15 01:23:31 +0200

    UART working nice

--- ARE/main.asm
+++ ARE/main.asm
@@ -4,7 +4,9 @@ l_reset:
 	SEI
 
 l_loop:	
+	UC_SR_I 'i'
 	UC_SR ds_out_l
+	UC_SR_I 'b'
 	UC_SR ds_out_r
 	rjmp l_loop
 
--- uart.py
+++ uart.py
@@ -32,14 +32,28 @@ def main():
         print("Usage:")
         print(__file__)
         print("     -h (--help)              Show this message")
+        print("     -g (--guide)             Show format string guide")
         print("     -p (--port=) <PORT>      Set port to connect to")
         print("     -f (--format=) <STRING>  Set format string ('" + ESCAPE_CHAR + "c' by default)")
         print("     -b (--baud=) <RATE>      Set baud rate (9600 by default)")
         print("     -l (--list)              List available serial devices")
         return
 
+    def printGuide():
+        print("Format string guide:")
+        print("     " + ESCAPE_CHAR + "b      Print next byte (eg. '01101000')")
+        print("     " + ESCAPE_CHAR + "c      Print next ascii character (eg. 'p')")
+        print("     " + ESCAPE_CHAR + "d      Print next 16-bit integer (eg. '2850')")
+        print("     " + ESCAPE_CHAR + "e      Interpret next byte as escape char")
+        print("     " + ESCAPE_CHAR + "h      Print next byte in hexadecimal base (eg. '7F')")
+        print("     " + ESCAPE_CHAR + "i      Print next 8-bit integer (eg. '126')")
+        print("     " + ESCAPE_CHAR + "n      New line")
+        print("     " + ESCAPE_CHAR + "t      Tabulation")
+        print("     " + ESCAPE_CHAR + "q      Ignore next byte")
+        return
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hp:f:o:m:b:l", ["help", "port=", "format=", "list"])
+        opts, args = getopt.getopt(sys.argv[1:], "hgp:f:o:m:b:l", ["help", "guide", "port=", "format=", "list"])
     except getopt.GetoptError as err:
         print(err)
         print()           
@@ -55,7 +69,9 @@ def main():
         if o in ("-h", "--help"):
             printUsage()
             print()
-            sys.exit(2)
+        elif o in ("-g", "--guide"):
+            printGuide();
+            print()
         elif o in ("-p", "--port"):
             ports = serial.tools.list_ports.comports()
             for p in ports:
@@ -122,33 +138,51 @@ def main():
         if not quitting:
             print("Data:")
 
+        def byteToInt(byte):
+            return int.from_bytes(byte, byteorder='big')
+
+        def intToBinStr(integer):
+            return bin(integer).lstrip('0b').zfill(8)
+
+        def printEscape(escape, recursionEnabled):
+            if escape == ESCAPE_CHAR:
+                sys.stdout.write(ESCAPE_CHAR)
+            elif escape == 'c':
+                sys.stdout.write(str(chr(byteToInt(ser.read()))))
+            elif escape == 'b':
+                sys.stdout.write(intToBinStr(byteToInt(ser.read())))
+            elif escape == 'e':
+                if recursionEnabled:
+                    printEscape(str(chr(byteToInt(ser.read()))), False)
+                else:
+                    sys.stdout.write("<RECURSIVE ESCAPE>")
+            elif escape == 'i':
+                sys.stdout.write(str(byteToInt(ser.read())))
+            elif escape == 'h':
+                sys.stdout.write(str(hex(byteToInt(ser.read())).lstrip('0x').zfill(2)))
+            elif escape == 'd':
+                lsb = byteToInt(ser.read())
+                msb = byteToInt(ser.read())
+                sys.stdout.write(str((msb << 8) | lsb))
+            elif escape == 't':
+                sys.stdout.write('\t')
+            elif escape == 'n':
+                sys.stdout.write('\n')
+            elif escape == 'q':
+                ser.read()
+            else:
+                sys.stdout.write("<BAD ESCAPE " + quote(ESCAPE_CHAR + escape) + ">")
+            return
+
         flist = format.split(ESCAPE_CHAR)
         prefix = flist[0]
         flist = flist[1:]
         while not quitting:
             try:
                 sys.stdout.write(prefix)
-                for tk in flist:
-                    escape = tk[0]
-                    if escape == ESCAPE_CHAR:
-                        sys.stdout.write(ESCAPE_CHAR)
-                    elif escape == 'c':
-                        sys.stdout.buffer.write(ser.read())
-                    elif escape == 'b':
-                        sys.stdout.write(bin(int.from_bytes(ser.read(), byteorder='big')).lstrip('0b').zfill(8))
-                    elif escape == 'i':
-                        sys.stdout.write(str(int.from_bytes(ser.read(), byteorder='big')))
-                    elif escape == 'd':
-                        lsb = bin(int.from_bytes(ser.read(), byteorder='big')).lstrip('0b').zfill(8)
-                        msb = bin(int.from_bytes(ser.read(), byteorder='big')).lstrip('0b').zfill(8)
-                        sys.stdout.write(msb + lsb)
-                    elif escape == 't':
-                        sys.stdout.write('\t')
-                    elif escape == 'n':
-                        sys.stdout.write('\n')
-                    else:
-                        sys.stdout.write("<BAD ESCAPE " + quote(ESCAPE_CHAR + escape) + ">")        
-                    sys.stdout.write(tk[1:])                
+                for token in flist:
+                    printEscape(token[0], True)
+                    sys.stdout.write(token[1:])
                 sys.stdout.flush()
             except Exception as err:
                 print()                
