5c31dc6853de72449d3364268676291de58e7935
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-08-25 19:06:08 +0200

    Buzzer working (bad code)

--- ARE/main.asm
+++ ARE/main.asm
@@ -1,12 +1,19 @@
 #ifdef CODE
 ;################## CODE ##################
-
+	
 l_reset:
-	SEI
+	sei
+	cli
+	BZ_SR_SQ_SET_TONE 0, 7, 30 / 1000.0, 40
+	BZ_SR_SQ_SET_MUTE 1, 7, 30 / 1000.0
+	BZ_SR_SQ_SET_TONE 2, 7, 30 / 1000.0, 50
+	BZ_SR_SQ_SET_MUTE 3, 7, 30 / 1000.0
+	BZ_SR_SQ_SET_TONE 4, 7, 30 / 1000.0, 60
+	BZ_SR_SQ_SET_MUTE 5, 7, 30 / 1000.0
+	BZ_SR_SQ_SET_TONE 6, 7, 30 / 1000.0, 100
+	BZ_SR_SQ_START 7
 
 l_loop:	
-	UC_SR_R DS_R_OUT_L
-	UC_SR_R DS_R_OUT_R
 	rjmp l_loop
 
 ;##########################################
