19e4ac26edde230206305d9e39ad0cd18cdaa98f
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-09-13 13:15:08 +0200

    Working

--- ARE/main.asm
+++ ARE/main.asm
@@ -6,12 +6,22 @@ l_reset:
 	ldi r16, 0
 
 l_loop:	
-	ldi r24, 1 << 3
-	ldi r25, 1 << 1
-	LM_SR_DRAW_COL r16, r24, r25
 
-	ldi  r18, 3
-    ldi  r19, 19
+	cli
+	lds r17, DS_R_OUT_L
+	cp r16, r17
+	brne lol
+	mov r24, r16
+	ser r25
+	rjmp go
+lol:
+	clr r24
+	clr r25
+go:
+	LM_SR_DRAW_COL r16, r24, r25
+	
+    ldi  r18, 11
+    ldi  r19, 99
 L1: dec  r19
     brne L1
     dec  r18
@@ -19,6 +29,7 @@ L1: dec  r19
 
 	inc r16
 	cpi r16, 16
+	sei
 	brne l_loop
 	rjmp l_reset
 
