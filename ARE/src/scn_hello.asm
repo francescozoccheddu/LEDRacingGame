304dc0740fbb16c8d1f1df8a015a9268d9ebc4ed
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-09-13 19:23:55 +0200

    Fixed bug

--- ARE/src/scn_hello.asm
+++ ARE/src/scn_hello.asm
@@ -2,7 +2,7 @@
 ;################## SETUP #################
 
 	ldi ZH, HIGH( sh_sr_draw )
-	ldi ZL, LOW( sh_sr_draw )
+	ldi ZL, LOW( sh_sr_draw)
 
 ;##########################################
 #endif
@@ -11,14 +11,16 @@
 ;################## CODE ##################
 
 sh_sr_draw:
-	cpi m_col, 6
-	breq sh_l_draw_dot
-	cpi m_col, 9
+	clr m_cl
+	clr m_ch
+	lds m_ch, DS_R_OUT_L
+	cp m_col, m_ch
 	brne sh_l_draw_ret
-sh_l_draw_dot:
 	ldi m_ch, (1 << 0) | (1 << 1) 
-	ldi m_cl, (1 << 6) | (1 << 7) 
+	ldi m_cl, (1 << 6) | (1 << 7)
+	ret
 sh_l_draw_ret:
+	clr m_ch
 	ret 
 
 ;##########################################
