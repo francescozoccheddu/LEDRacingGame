56eaef39fd941fa073bc8fa82fa8c3fab0cb07cf
francescozoccheddu <zoccheddu.fra@gmail.com>
2017-09-18 14:28:20 +0200

    Before cleanup

--- ARE/src/scn_hello.asm
+++ ARE/src/scn_hello.asm
@@ -12,14 +12,10 @@
 
 sh_sr_draw:
 	clr m_cl
-	clr m_ch
 	lds m_ch, DS_R_OUT_L
 	cp m_col, m_ch
-	brne sh_l_draw_ret
-	ldi m_ch, (1 << 0) | (1 << 1) 
-	ldi m_cl, (1 << 6) | (1 << 7)
-	ret
-sh_l_draw_ret:
+	brne PC + 2
+	ser m_cl
 	clr m_ch
 	ret 
 
