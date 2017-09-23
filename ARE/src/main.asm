9d82d8771b75bd0ceba481decc088fcac7771521
francescozoccheddu <zoccheddu.fra@gmail.com>
2017-09-23 22:40:07 +0200

    Cleanup

---
+++ ARE/src/main.asm
@@ -0,0 +1,40 @@
+; Francesco Zoccheddu
+; ARE
+; main
+
+; assembler setup
+
+.cseg
+.nooverlap
+
+.include "m2560def.inc"
+
+.equ FOSC = 16000000
+
+.macro INTVEC
+	.set INTVECM_PC = PC
+	.org @0
+		jmp @1
+	.org INTVECM_PC
+.endmacro
+
+.include "builtin_led.asm"
+
+.org INT_VECTORS_SIZE
+
+; main
+
+INTVEC 0, m_l_reset
+
+.def m_tmp = r16
+
+m_l_reset:
+	; setup stack
+	ldi m_tmp, HIGH(RAMEND)
+	out SPH, m_tmp
+	ldi m_tmp, LOW(RAMEND)
+	out SPL, m_tmp
+	;setup modules
+	;setup builtin LED
+	BL_SRC_SETUP m_tmp
+	rjmp m_l_reset
