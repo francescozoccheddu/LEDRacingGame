845f7cf861bba7571a45e5d7c1cf32d420cc3d44
francescozoccheddu <zoccheddu.fra@gmail.com>
2017-09-29 20:38:18 +0200

    BL and LM OK

--- ARE/src/led_matrix.asm
+++ ARE/src/led_matrix.asm
@@ -1,6 +1,7 @@
 ; Francesco Zoccheddu
 ; ARE
 ; LED matrix
+; dirty PORTC, DDRC, PINC
 
 .equ LM_PORT = PORTC
 .equ LM_DDR = DDRC
@@ -10,43 +11,45 @@
 .equ LM_BIT_CLK = 6 ;digital pin 31
 .equ LM_BIT_LAT = 7 ;digital pin 30
 
+; [SOURCE] setup
 ; @0 (dirty immediate register)
 .macro LM_SRC_SETUP
 	ser @0
 	out LM_DDR, @0
 .endmacro
 
-; @0 (dirty immediate register)
-; @1 (dirty immediate register)
-; @2 (column data high register)
-; @3 (column data low register)
-; @4 (column index register)
+; [SOURCE] send column data '@0:@1' with column index '@2' 
+; @0 (column data high register)
+; @1 (column data low register)
+; @2 (column index register)
+; @3 (dirty immediate register)
+; @4 (dirty immediate register)
 .macro LM_SRC_SEND_COL
-	ldi @0, 16
+	ldi @3, 16
 	;row
-	ldi @1, (1 << LM_BIT_G) | (1 << LM_BIT_DI)
-	lsr @2
-	ror @3
+	ldi @4, (1 << LM_BIT_G) | (1 << LM_BIT_DI)
+	lsr @0
+	ror @1
 	brcc PC + 2
-	ldi @1, (1 << LM_BIT_G)
-	out LM_PORT, @1
-	ori @1, 1 << LM_BIT_CLK
-	out LM_PORT, @1
-	andi @1, ~(1 << LM_BIT_CLK)
-	out LM_PORT, @1
+	ldi @4, (1 << LM_BIT_G)
+	out LM_PORT, @4
+	ori @4, 1 << LM_BIT_CLK
+	out LM_PORT, @4
+	andi @4, ~(1 << LM_BIT_CLK)
+	out LM_PORT, @4
 	;loop
-	dec @0
+	dec @3
 	brne PC - 11
 	;send LAT
-	ldi @1, (1 << LM_BIT_G) | (1 << LM_BIT_LAT)
-	out LM_PORT, @1
-	ldi @1, (1 << LM_BIT_G)
-	out LM_PORT, @1
-	;send col
-	or @1, @4
-	out LM_PORT, @1
-	;end G
+	ldi @4, (1 << LM_BIT_G) | (1 << LM_BIT_LAT)
 	out LM_PORT, @4
+	ldi @4, (1 << LM_BIT_G)
+	out LM_PORT, @4
+	;send col
+	or @4, @2
+	out LM_PORT, @4
+	;end G
+	out LM_PORT, @2
 .endmacro
 
 
