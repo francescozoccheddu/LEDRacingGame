5ebd97ff0549a29d75e41c42ce924a1ccb25797a
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-10-01 18:49:54 +0200

    Serial programming working

---
+++ ARE/src/serial_prog.asm
@@ -0,0 +1,76 @@
+; Francesco Zoccheddu
+; ARE
+; serial programming
+
+#define _sp_data ric
+#define _sp_tmp rib
+
+_sp_sr_ut:
+	UC_SRC_T _sp_data, _sp_tmp
+	ret
+
+_sp_sr_ur:
+	UC_SRC_R _sp_data
+	ret
+
+#define _SP_OPCODE_MSK 0b00111111
+#define _SP_OPCODE_R_W 7
+#define _SP_OPCODE_EEPR_RAM 6
+
+#define _sp_b1 ri0
+#define _sp_b2 ria
+
+ISR UC_RCOMPLETE_INTaddr
+	BL_SRC_ON rid
+	; store bit 1 in 'b1'
+	rcall _sp_sr_ur 
+	mov _sp_b1, _sp_data
+	; store bit 2 in 'b2'
+	rcall _sp_sr_ur 
+	mov _sp_b2, _sp_data
+	; eventually store bit 3 in 'data'
+	sbrc _sp_b1, _SP_OPCODE_R_W
+	rcall _sp_sr_ur
+	BL_SRC_OFF rid
+	; store address h in 'tmp'
+	mov _sp_tmp, _sp_b1
+	andi _sp_tmp, _SP_OPCODE_MSK
+	sbrc _sp_b1, _SP_OPCODE_EEPR_RAM
+	rjmp _sp_l_isr_incoming_addr_ram
+	; EEPROM address
+	EP_SRC_WAIT
+	EP_SRC_ADDR _sp_b2, _sp_tmp
+	sbrc _sp_b1, _SP_OPCODE_R_W
+	rjmp _sp_l_isr_incoming_eeprom_write
+	; EEPROM read
+	EP_SRC_FREAD _sp_data
+	rcall _sp_sr_ut
+	reti
+_sp_l_isr_incoming_eeprom_write:
+	; EEPROM write
+	EP_SRC_FWRITE _sp_data
+	reti
+_sp_l_isr_incoming_addr_ram:
+	; SRAM address
+	push ZL
+	push ZH
+	movw ZH:ZL, _sp_tmp:_sp_b2
+	sbrc _sp_b1, _SP_OPCODE_R_W
+	rjmp _sp_l_isr_incoming_sram_write
+	; EEPROM read
+	ld _sp_data, Z
+	rcall _sp_sr_ut
+	rjmp _sp_l_isr_incoming_sram_done
+_sp_l_isr_incoming_sram_write:
+	; EEPROM write
+	st Z, _sp_data
+_sp_l_isr_incoming_sram_done:
+	pop ZH
+	pop ZL
+	reti
+
+#undef _sp_b1
+#undef _sp_b2
+
+#undef _sp_data
+#undef _sp_tmp
--- ARE/src/uart_comm.asm
+++ ARE/src/uart_comm.asm
@@ -14,41 +14,72 @@
 .equ UC_TREADY_INTaddr = UDRE0addr
 .equ UC_RCOMPLETE_INTaddr = URXC0addr
 
+#define _uc_tmp @0
+
 ; [SOURCE] setup
 ; @0 (dirty immediate register)
 .macro UC_SRC_SETUP
 	; set UBRRR
-	ldi @0, HIGH( _UC_UBRR )
-	sts UBRR0H, @0
-	ldi @0, LOW( _UC_UBRR )
-	sts UBRR0L, @0
+	ldi _uc_tmp, HIGH( _UC_UBRR )
+	sts UBRR0H, _uc_tmp
+	ldi _uc_tmp, LOW( _UC_UBRR )
+	sts UBRR0L, _uc_tmp
 	; set UCSRA
-	ldi @0, _UC_UCSRA_VAL
-	sts UCSR0A, @0
+	ldi _uc_tmp, _UC_UCSRA_VAL
+	sts UCSR0A, _uc_tmp
 	; set UCSRB
-	ldi @0, _UC_UCSRB_VAL
-	sts UCSR0B, @0
+	ldi _uc_tmp, _UC_UCSRB_VAL
+	sts UCSR0B, _uc_tmp
 	; set UCSRC
-	ldi @0, _UC_UCSRC_VAL
-	sts UCSR0C, @0
+	ldi _uc_tmp, _UC_UCSRC_VAL
+	sts UCSR0C, _uc_tmp
 .endmacro
 
-; [SOURCE] transmit '@0' data without checking whether the buffer is empty
+#undef _uc_tmp
+
+#define _uc_data @0
+#define _uc_tmp @1
+
+; [SOURCE] transmit '_uc_data' data without checking whether the buffer is empty
 ; @0 (data register)
 .macro UC_SRC_FT
-	sts UDR0, @0
+	sts UDR0, _uc_data
 .endmacro
 
-; [SOURCE] receive to '@0' register without checking whether the buffer is not empty
+; [SOURCE] receive to '_uc_data' register without checking whether the buffer is not empty
 ; @0 (data register)
 .macro UC_SRC_FR
-	lds @0, UDR0
+	lds _uc_data, UDR0
+.endmacro
+
+; [SOURCE] transmit '_uc_data' data as soon as the buffery is empty
+; @0 (data register)
+; @1 (dirty immediate register)
+.macro UC_SRC_T
+_uc_l_src_t:
+	lds _uc_tmp, UCSR0A
+	sbrs _uc_tmp, UDRE0
+	rjmp _uc_l_src_t
+	UC_SRC_FT _uc_data
+.endmacro
+
+; [SOURCE] receive to '_uc_data' register as soon as the buffer is not empty
+; @0 (data register)
+.macro UC_SRC_R
+_uc_l_src_r:
+	lds _uc_data, UCSR0A
+	sbrs _uc_data, RXC0
+	rjmp _uc_l_src_r
+	UC_SRC_FR _uc_data
 .endmacro
 
 ; [SOURCE] enable / disable transmit ready interrupt
 ; @0 (boolean interrupt state)
 ; @1 (dirty immediate register)
 .macro UC_SRC_TREADY_INTE
-	ldi @1, @0 ? ( _UC_UCSRB_VAL | _UC_UDRE_VAL ) : _UC_UCSRB_VAL
-	sts UCSR0B, @1
+	ldi _uc_tmp, _uc_data ? ( _UC_UCSRB_VAL | _UC_UDRE_VAL ) : _UC_UCSRB_VAL
+	sts UCSR0B, _uc_tmp
 .endmacro
+
+#undef _uc_data
+#undef _uc_tmp
