867b6d58ca4a411809516c3c9f8fd39243cd46e6
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-10-01 12:49:05 +0200

    DS full working

--- ARE/src/builtin_led.asm
+++ ARE/src/builtin_led.asm
@@ -10,48 +10,50 @@ IO_DEF _BL, _BL_IO
 
 .equ _BL_BACKUP_EXTRA_PINS = 0
 
+#define _bl_tmp @0
+
 ; [SOURCE] setup
 ; @0 (dirty immediate register)
 .macro BL_SRC_SETUP
 	; set data direction register to output for LED pin
 	.if _BL_BACKUP_EXTRA_PINS
-		in @0, _BL_DDR
-		ori @0, 1 << _BL_BIT
+		in _bl_tmp, _BL_DDR
+		ori _bl_tmp, 1 << _BL_BIT
 	.else
-		ldi @0, 1 << _BL_BIT
+		ldi _bl_tmp, 1 << _BL_BIT
 	.endif
-	out _BL_DDR, @0
+	out _BL_DDR, _bl_tmp
 .endmacro
 
 ; [SOURCE] turn builtin LED on
 ; @0 (dirty immediate register)
 .macro BL_SRC_ON
 	.if _BL_BACKUP_EXTRA_PINS
-		in @0, _BL_PORT
-		ori @0, 1 << _BL_BIT
+		in _bl_tmp, _BL_PORT
+		ori _bl_tmp, 1 << _BL_BIT
 	.else
-		ldi @0, 1 << _BL_BIT
+		ldi _bl_tmp, 1 << _BL_BIT
 	.endif
-	out _BL_PORT, @0
+	out _BL_PORT, _bl_tmp
 .endmacro
 
 ; [SOURCE] turn builtin LED off
 ; @0 (dirty immediate register)
 .macro BL_SRC_OFF
 	.if _BL_BACKUP_EXTRA_PINS
-		in @0, _BL_PORT
-		andi @0, ~(1 << _BL_BIT)
+		in _bl_tmp, _BL_PORT
+		andi _bl_tmp, ~(1 << _BL_BIT)
 	.else
-		clr @0
+		clr _bl_tmp
 	.endif
-	out _BL_PORT, @0
+	out _BL_PORT, _bl_tmp
 .endmacro
 
 ; [SOURCE] toggle builtin LED state
 ; @0 (dirty immediate register)
 .macro BL_SRC_TOGGLE
-	ldi @0, 1 << _BL_BIT
-	out _BL_PIN, @0
+	ldi _bl_tmp, 1 << _BL_BIT
+	out _BL_PIN, _bl_tmp
 .endmacro
 
-
+#undef _bl_tmp
--- ARE/src/distance_sens.asm
+++ ARE/src/distance_sens.asm
@@ -43,126 +43,200 @@ IO_DEF _DS, _DS_IO
 	; set data direction register to output for trig pin
 	ldi @0, 1 << _DS_TRIG_BIT
 	sts _DS_DDR, @0
+	PARS @0
 .endmacro
 
-#define _DS_OUT intr4
+#define DS_EMAX 300
+#define DS_MAX 250
+#define DS_MIN 70
 
-; [SOURCE] clamp input '@0:@1' between '@2:@3' and '@4:@5' and return '_DS_OUT' between '@6' and '@7'
-; @0 (input h)
-; @1 (input l)
-; @2 (input bottom l)
-; @3 (input bottom h)
-; @4 (input top h)
-; @5 (input top l)
-; @6 (output bottom)
-; @7 (output high)
-; @8 (dirty register)
-; @9 (dirty register)
-.macro DS_SRC_CLAMP
-	.equ _DS_SRC_CLAMP_BACKUP_CURS = 1
-	.if _DS_SRC_CLAMP_BACKUP_CURS
-		push @8
-		push @9
-	.endif
-	movw @8:@9, @4:@5
-	mov _DS_OUT, @7
-_ds_l_sr_clamp_loop:
-	sub @7, @6
-	dec @7
-	breq _ds_l_sr_clamp_return
-	add @7, @6
-	inc @7
-	cp @9, @1
-	cpc @8, @0
-	breq _ds_l_sr_clamp_return
-	brsh _ds_l_sr_clamp_smaller
-_ds_l_sr_clamp_greater:
-	movw @2:@3, @8:@9
-	mov @6, _DS_OUT
-	add @9, @5
-	adc @8, @4
-	ror @8
-	ror @9
-	add _DS_OUT, @7
-	ror _DS_OUT
-	rjmp _ds_l_sr_clamp_loop
-_ds_l_sr_clamp_smaller:
-	movw @4:@5, @8:@9
-	mov @7, _DS_OUT
-	add @9, @3
-	adc @8, @2
-	ror @8
-	ror @9
-	add _DS_OUT, @6
-	ror _DS_OUT
-	rjmp _ds_l_sr_clamp_loop
-_ds_l_sr_clamp_stop:
-	.if _DS_SRC_CLAMP_BACKUP_CURS
-		pop @9
-		pop @8
-	.endif
+.dseg
+_ds_ram_in_lol: .byte 1
+_ds_ram_in_loh: .byte 1
+_ds_ram_in_hil: .byte 1
+_ds_ram_in_hih: .byte 1
+_ds_ram_in_ehil: .byte 1
+_ds_ram_in_ehih: .byte 1
+ds_ram_out_val: .byte 1
+ds_ram_out_state: .byte 1
+.cseg
+
+.macro PARS
+	ldi @0, LOW( DS_MIN )
+	sts _ds_ram_in_lol, @0
+	ldi @0, HIGH( DS_MIN )
+	sts _ds_ram_in_loh, @0
+	ldi @0, LOW( DS_MAX )
+	sts _ds_ram_in_hil, @0
+	ldi @0, HIGH( DS_MAX )
+	sts _ds_ram_in_hih, @0
+	ldi @0, LOW( DS_EMAX )
+	sts _ds_ram_in_ehil, @0
+	ldi @0, HIGH( DS_EMAX )
+	sts _ds_ram_in_ehih, @0
 .endmacro
+
 
 ISR _DS_OCAaddr
 ds_isr_trig:
-	; start trig
-	ldi intr0, 1 << _DS_TRIG_BIT
-	sts _DS_PORT, intr0
-	; stop timer
-	clr intr0
-	sts _DS_TCCRB, intr0
-	; clear counter
-	sts _DS_TCNTH, intr0
-	sts _DS_TCNTL, intr0
-	
-    ldi  intr0, 53
-L1: dec  intr0
-    brne L1
-    nop
 
+#define _ds_tmp ria
+	; start trig
+	ldi _ds_tmp, 1 << _DS_TRIG_BIT
+	sts _DS_PORT, _ds_tmp
+	; stop timer
+	clr _ds_tmp
+	sts _DS_TCCRB, _ds_tmp
+	; clear counter
+	sts _DS_TCNTH, _ds_tmp
+	sts _DS_TCNTL, _ds_tmp
+	; skip if not measured
+	lds _ds_tmp, _DS_TIMSK
+	sbrc _ds_tmp, ICIE
+	rjmp _ds_isr_trig_bad
+#undef _ds_tmp
+
+	; skip if greater than max
+#define _ds_cursl ria
+#define _ds_cursh rib
+#define _ds_inl ri0
+#define _ds_inh ri1
+	lds _ds_cursl, _ds_ram_in_ehil
+	lds _ds_cursh, _ds_ram_in_ehih
+	lds _ds_inl, _ds_ram_ltimel
+	lds _ds_inh, _ds_ram_ltimeh
+	cp _ds_inl, _ds_cursl
+	cpc _ds_inh, _ds_cursh
+	brsh _ds_isr_trig_bad
+
+	; clamp
+	; setup clamp parameters
+#define _ds_in_lol ri2
+#define _ds_in_loh ri3
+#define _ds_in_hil ri4
+#define _ds_in_hih ri5
+#define _ds_out_lo ric
+#define _ds_out_hi rid
+#define _ds_out ri6
+	lds _ds_in_lol, _ds_ram_in_lol
+	lds _ds_in_loh, _ds_ram_in_loh
+	lds _ds_in_hil, _ds_ram_in_hil
+	lds _ds_in_hih, _ds_ram_in_hih
+	clr _ds_out_lo
+	ser _ds_out_hi
+	; start clamping
+_ds_l_isr_trig_clamp_start:
+	movw _ds_cursh:_ds_cursl, _ds_in_hih:_ds_in_hil
+	mov _ds_out, _ds_out_hi
+_ds_l_isr_trig_clamp_loop:
+	sub _ds_out_hi, _ds_out_lo
+	breq _ds_l_isr_trig_clamp_stop
+	dec _ds_out_hi
+	breq _ds_l_isr_trig_clamp_stop
+	add _ds_out_hi, _ds_out_lo
+	inc _ds_out_hi
+	cp _ds_cursl, _ds_inl
+	cpc _ds_cursh, _ds_inh
+	breq _ds_l_isr_trig_clamp_stop
+	brsh _ds_l_isr_trig_clamp_smaller
+_ds_l_isr_trig_clamp_greater:
+	movw _ds_in_loh:_ds_in_lol, _ds_cursh:_ds_cursl
+	mov _ds_out_lo, _ds_out
+	add _ds_cursl, _ds_in_hil
+	adc _ds_cursh, _ds_in_hih
+	ror _ds_cursh
+	ror _ds_cursl
+	add _ds_out, _ds_out_hi
+	ror _ds_out
+	rjmp _ds_l_isr_trig_clamp_loop
+_ds_l_isr_trig_clamp_smaller:
+	movw _ds_in_hih:_ds_in_hil, _ds_cursh:_ds_cursl
+	mov _ds_out_hi, _ds_out
+	add _ds_cursl, _ds_in_lol
+	adc _ds_cursh, _ds_in_loh
+	ror _ds_cursh
+	ror _ds_cursl
+	add _ds_out, _ds_out_lo
+	ror _ds_out
+	rjmp _ds_l_isr_trig_clamp_loop
+#undef _ds_cursl
+#undef _ds_cursh
+#undef _ds_inl
+#undef _ds_inh
+#undef _ds_in_lol
+#undef _ds_in_loh
+#undef _ds_in_hil
+#undef _ds_in_hih
+#undef _ds_out_lo
+#undef _ds_out_hi
+
+#define _ds_tmp ria
+_ds_l_isr_trig_clamp_stop:
+	; write output value
+	sts ds_ram_out_val, _ds_out
+	; set output state to true
+	ser _ds_tmp
+	rjmp _ds_isr_trig_done
+#undef _ds_out
+
+_ds_isr_trig_bad:
+	; set output state to false
+	clr _ds_tmp
+_ds_isr_trig_done:
+	; write output state
+	sts ds_ram_out_state, _ds_tmp
 	; enable interrupts
-	ldi intr0, ICIE_VAL | OCIEA_VAL
-	sts _DS_TIMSK, intr0
+	ldi _ds_tmp, ICIE_VAL | OCIEA_VAL
+	sts _DS_TIMSK, _ds_tmp
 	; stop trig
-	clr intr0
-	sts _DS_PORT, intr0
+	clr _ds_tmp
+	sts _DS_PORT, _ds_tmp
 	; start timer
-	ldi intr0, CS_VAL(_DS_CS) | ICES_VAL | ICN_VAL
-	sts _DS_TCCRB, intr0
+	ldi _ds_tmp, CS_VAL(_DS_CS) | ICES_VAL | ICN_VAL
+	sts _DS_TCCRB, _ds_tmp
 	reti
+#undef _ds_tmp
 
 .dseg
-_ds_ram_ltimeh: .byte 1
 _ds_ram_ltimel: .byte 1
+_ds_ram_ltimeh: .byte 1
 .cseg
 
 ISR _DS_ICPaddr
+#define _ds_icrl ri0
+#define _ds_icrh ri1
+#define _ds_tmp1 ria
+#define _ds_tmp2 rib
 	; load ICR
-	lds intr0, _DS_ICRL
-	lds intr1, _DS_ICRH
+	lds _ds_icrl, _DS_ICRL
+	lds _ds_icrh, _DS_ICRH
 	; check if rising or falling edge
-	lds intr2, _DS_TCCRB
-	sbrs intr2, ICES
+	lds _ds_tmp1, _DS_TCCRB
+	sbrs _ds_tmp1, ICES
 	rjmp _ds_l_isr_icp_falling
 _ds_l_isr_icp_rising:
 	; save ICR to sram
-	sts _ds_ram_ltimeh, intr1
-	sts _ds_ram_ltimel, intr0
+	sts _ds_ram_ltimel, _ds_icrl
+	sts _ds_ram_ltimeh, _ds_icrh
 	; set input capture to falling edge
-	lds intr0, _DS_TCCRB
-	andi intr0, ~(ICES_VAL)
-	sts _DS_TCCRB, intr0
+	lds _ds_tmp1, _DS_TCCRB
+	andi _ds_tmp1, ~(ICES_VAL)
+	sts _DS_TCCRB, _ds_tmp1
 	reti
 _ds_l_isr_icp_falling:
 	; save difference to sram
-	lds intr2, _ds_ram_ltimel
-	lds intr3, _ds_ram_ltimeh
-	sub intr0, intr2
-	sbc intr1, intr3
-	sts _ds_ram_ltimeh, intr1
-	sts _ds_ram_ltimel, intr0
+	lds _ds_tmp1, _ds_ram_ltimel
+	lds _ds_tmp2, _ds_ram_ltimeh
+	sub _ds_icrl, _ds_tmp1
+	sbc _ds_icrh, _ds_tmp2
+	sts _ds_ram_ltimel, _ds_icrl
+	sts _ds_ram_ltimeh, _ds_icrh
 	; disable input capture interrupt
-	ldi intr0, OCIEA_VAL
-	sts _DS_TIMSK, intr0
+	ldi _ds_tmp1, OCIEA_VAL
+	sts _DS_TIMSK, _ds_tmp1
 	reti
+#undef _ds_icrh
+#undef _ds_icrl
+#undef _ds_tmp1
+#undef _ds_tmp2
 
