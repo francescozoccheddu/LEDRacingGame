8b253a983dff0e74a234b98a631bde1bed701f12
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2017-10-07 21:50:59 +0200

    Smooth working

--- ARE/src/game.asm
+++ ARE/src/game.asm
@@ -12,11 +12,11 @@ _g_ram_frame: .byte 16*2
 .endmacro
 
 .macro G_SRC_SPLOAD
-#define ML_SMOOTH_SLOW 0.85
-#define ML_SMOOTH 0.6
-	ldi rma, int(ML_SMOOTH * 255)
+#define ML_SMOOTH 8
+#define ML_SMOOTH_SLOW 3
+	ldi rma, ML_SMOOTH
 	sts _g_ram_smooth, rma
-	ldi rma, int(ML_SMOOTH_SLOW * 255)
+	ldi rma, ML_SMOOTH_SLOW
 	sts _g_ram_smooth_slow, rma
 .endmacro
 
@@ -26,18 +26,27 @@ _g_ram_frame: .byte 16*2
 #define _g_tmp3 ml_tmp3
 #define _g_tmp4 ml_tmp4
 
-; mull (a) (dirty)
-; tmp4 (b)
-; tmp3 (factor) (dirty)
-; mulh (out)
+; tmp1 (a)
+; tmp2 (b)
+; tmp3 (prog)
 _g_l_smooth:
-	mul mull, _g_tmp3
-	movw _g_tmp2:_g_tmp1, mulh:mull
-	com _g_tmp3
-	mov mull, _g_tmp4
-	mul mull, _g_tmp3
-	add mull, _g_tmp1
-	adc mulh, _g_tmp2
+	cp _g_tmp1, _g_tmp2
+	brsh _g_l_smooth_greater
+	mov _g_tmp4, _g_tmp2
+	sub _g_tmp4, _g_tmp1
+	cp _g_tmp4, _g_tmp3
+	brlo _g_l_smooth_clamp
+	add _g_tmp1, _g_tmp3
+	ret
+_g_l_smooth_greater:
+	mov _g_tmp4, _g_tmp1
+	sub _g_tmp4, _g_tmp2
+	cp _g_tmp4, _g_tmp3
+	brlo _g_l_smooth_clamp
+	sub _g_tmp1, _g_tmp3
+	ret
+_g_l_smooth_clamp:
+	mov _g_tmp1, _g_tmp2
 	ret
 
 g_l_update:
@@ -45,32 +54,30 @@ g_l_update:
 	lds _g_tmp1, ds_ram_out_state
 	tst _g_tmp1
 	breq _g_l_update_smooth_zombie
-	lds _g_tmp4, ds_ram_out_val
+	lds _g_tmp2, ds_ram_out_val
 	; svals = svals * smooths + rval * (1-smooths)
-	lds mull, _g_ram_dsval_slow
+	lds _g_tmp1, _g_ram_dsval_slow
 	lds _g_tmp3, _g_ram_smooth_slow
 	rcall _g_l_smooth
-	sts _g_ram_dsval_slow, mulh
+	sts _g_ram_dsval_slow, _g_tmp1
 	; sval = sval * smooth + rval * (1-smooth)
-	lds mull, _g_ram_dsval
+	lds _g_tmp1, _g_ram_dsval
 	lds _g_tmp3, _g_ram_smooth
 	rcall _g_l_smooth
-	sts _g_ram_dsval, mulh
+	sts _g_ram_dsval, _g_tmp1
 	rjmp _g_l_update_smooth_done
 _g_l_update_smooth_zombie:
 	; sval = sval * smooth + svals * (1-smooth)
-	lds _g_tmp4, _g_ram_dsval_slow
-	lds mull, _g_ram_dsval
+	lds _g_tmp1, _g_ram_dsval
+	lds _g_tmp2, _g_ram_dsval_slow
 	lds _g_tmp3, _g_ram_smooth
 	rcall _g_l_smooth
-	sts _g_ram_dsval, mulh
+	sts _g_ram_dsval, _g_tmp1
 _g_l_update_smooth_done:
-	com mulh
-	lsr mulh
-	lsr mulh
-	lsr mulh
-	lsr mulh
-	sts _g_ram_col, mulh
+	com _g_tmp1
+	swap _g_tmp1
+	andi _g_tmp1, 0b00001111
+	sts _g_ram_col, _g_tmp1
 	rjmp g_l_update_done
 
 #define _g_col ml_col
@@ -86,7 +93,7 @@ g_l_draw:
 	ld _g_cl, Z+
 	ld _g_ch, Z
 	; add ship
-	lds _g_tmp1, _g_ram_dsval_slow
+/*	lds _g_tmp1, _g_ram_dsval_slow
 	com _g_tmp1
 	lsr _g_tmp1
 	lsr _g_tmp1
@@ -94,10 +101,13 @@ g_l_draw:
 	lsr _g_tmp1
 	cp _g_col, _g_tmp1
 	brne PC + 2
-	ori _g_cl, 2
+	ori _g_cl, 4
+	;delete*/
 	lds _g_tmp1, _g_ram_col
 	cp _g_col, _g_tmp1
 	brne _g_l_draw_done
+	sbrc _g_ch, 7
+	nop ; lose
 	ori _g_cl, 1
 _g_l_draw_done:
 	rjmp g_l_draw_done
