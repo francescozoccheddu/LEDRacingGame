07bf82bd96bd08b9635be6cec89b5329d3931a7a
francescozoccheddu <zoccheddu.fra@gmail.com>
2017-10-22 13:43:54 +0200

    b2b batch

---
+++ ARE/bitmaps/bitmap2bytes.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import glob
+import math
+import os
+
+def convertImage(filename):
+    imgf = Image.open(filename)
+    img = imgf.convert("L")
+    imgf.close()
+
+    sx = img.size[0]
+    sy = img.size[1]
+
+    out = ".db "
+    
+    for x in range(sx):
+        for by in range(math.ceil(sy / 8)):
+            bout = "0b"
+            for bi in range(8):
+                y = by * 8 + bi
+                bout += "1" if y < sy and img.getpixel((x,y)) > 127 else "0"
+            out += bout + ","
+
+    out = out.rstrip(",") + "\r\n"
+
+    return out
+
+def main():
+    for file in glob.glob("./*.png"):
+        outfile = open(os.path.splitext(file)[0] + ".asm", 'w')
+        outfile.write(convertImage(file))
+        outfile.close()
+
+     
+if __name__ == "__main__":
+    main()
